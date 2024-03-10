e847dbd52cac911902740c9d266c562262c07815
RaishAnwar <raishkhan970@gmail.com>
2024-03-10 13:07:57 +0530

    complete post page functionality

--- client-blog/src/App.jsx
+++ client-blog/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute"
 import CreatePost from "./pages/CreatePost"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import UpdatePost from "./pages/UpdatePost"
+import PostPage from "./pages/postPage"
 
 const App = () => {
   return (
@@ -31,6 +32,8 @@ const App = () => {
         </Route>
        
         <Route path="/projects" element ={<Projects/>}/>
+        <Route path="/post/:postSlug" element ={<PostPage/>}/>
+
      </Routes>
 
      <Footer/>

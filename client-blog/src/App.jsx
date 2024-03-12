906f0b292c076c81966a1a27722df9bda5266bdd
RaishAnwar <raishkhan970@gmail.com>
2024-03-13 00:22:33 +0530

    Complete search and Home functionality

--- client-blog/src/App.jsx
+++ client-blog/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import UpdatePost from "./pages/UpdatePost"
 import PostPage from "./pages/postPage"
 import ScrollToTop from "./components/ScrollToTop"
+import Search from "./pages/Search"
 
 const App = () => {
   return (
@@ -24,6 +25,8 @@ const App = () => {
         <Route path="/about" element ={<About/>}/>
         <Route path="/sign-in" element ={<SignIn/>}/>
         <Route path="/sign-up" element ={<SignUp/>}/>
+        <Route path="/search" element ={<Search/>}/>
+
         <Route element={<PrivateRoute/>}>
         <Route path="/dashboard" element ={<Dashboard/>}/>
         </Route>
